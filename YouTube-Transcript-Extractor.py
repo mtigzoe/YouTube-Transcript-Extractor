6ce309db6a4a481d1eab30d589dcefc5fb871a0e
mtigzoe <mred5248@gmail.com>
2024-03-07 18:50:20 -0500

    Updated the file.

--- YouTube-Transcript-Extractor.py
+++ YouTube-Transcript-Extractor.py
@@ -1,5 +1,5 @@
-from langchain.document_loaders import YoutubeLoader
-from langchain.docstore.document import Document
+from langchain_community.document_loaders import YoutubeLoader
+from langchain_community.docstore.document import Document
 import textwrap
 
 url = input("Paste your YouTube url: ")
@@ -15,11 +15,13 @@ for doc in docs:
         metadata = doc.metadata 
         )
     del document.metadata['source']
-
-    print(document.metadata)
+    
     filename = document.metadata['title'].replace(' ', '_')
     wrapped_text = textwrap.fill(document.page_content, width=80) 
 
     with open(filename + ".txt", "w", encoding="utf-8") as file:
-    
+
+        file.write(str(document.metadata["title"]) + "\n")
+        file.write(str(document.metadata["author"]) + "\n")        
+        file.write("\n")
         file.write(str(wrapped_text))
